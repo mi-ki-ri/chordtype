02ec96274437ce2267976041059b94382c3e3114
hotta <hotta.tatsuya.fcc@gmail.com>
2020-02-17 15:06:28 +0900

    title and line

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,79 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        chordtype
-      </h1>
-      <h2 class="subtitle">
-        My classy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <b-container fluid>
+    <b-container tag="header" class="header" fluid>
+      <b-row>
+        <b-col>
+          <h1 class="text-center text-white">
+            ChordType
+          </h1>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-textarea
+            v-model="editorText"
+            class="editor m-1"
+          >
+          </b-textarea>
+        </b-col>
+        <b-col>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="2100"
+            height="2970"
+            class="svg m-1"
+          >
+            <text x="0" :y="(20 * index)+ 28" :key="index" v-for="(line, index) of formatted" :font-size="line.size">
+              {{line.text}}
+            </text>
+          </svg>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      editorText: "",
+    };
+  },
+  computed:{
+    
+    formatted(){
+      let splitted = this.editorText.split("\n")
+      var arrayForReturn = []
+
+      splitted.forEach((line,index,array) =>{
+        let text = line
+        let size = index != 0 ? 16 : 24
+
+
+        arrayForReturn.push({
+          text: text,
+          size: size
+        })
+      })
+      return arrayForReturn
+    }
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.header {
+  background-color: var(--cyan);
+  height: 8vh;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.editor,
+.canvas,
+.svg {
+  width: 100%;
+  height: 88vh;
+  border: 1px solid #ccc;
 }
 </style>
