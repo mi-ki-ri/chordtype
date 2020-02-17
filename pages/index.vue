a4ad26df122e0e941f1c5d6e781d6f51055e6eab
hotta <hotta.tatsuya.fcc@gmail.com>
2020-02-17 16:45:17 +0900

    b-col version

--- pages/index.vue
+++ pages/index.vue
@@ -9,26 +9,45 @@
         </b-col>
       </b-row>
     </b-container>
-    <b-container>
+    <b-container fluid>
       <b-row>
-        <b-col>
-          <b-textarea
-            v-model="editorText"
-            class="editor m-1"
-          >
-          </b-textarea>
+        <b-col class="editorArea m-2">
+          <b-input type="text" v-model="editorTitle" />
+          <b-textarea v-model="editorText" class="editor"> </b-textarea>
         </b-col>
-        <b-col>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="2100"
-            height="2970"
-            class="svg m-1"
-          >
-            <text x="0" :y="(20 * index)+ 28" :key="index" v-for="(line, index) of formatted" :font-size="line.size">
-              {{line.text}}
-            </text>
-          </svg>
+        <b-col class="visualArea m-2">
+          <b-container>
+            <b-row>
+              <b-col>
+                <h2>
+                  {{ editorTitle }}
+                </h2>
+              </b-col>
+            </b-row>
+            <b-row :key="index" v-for="(line, index) of formattedObj">
+              <b-col class="bar" :key="bindex" v-for="(bar, bindex) of line">
+                <b-row class="chords">
+                  <b-col
+                    class="chord"
+                    :key="cindex"
+                    v-for="(ch, cindex) of bar.chords"
+                  >
+                    {{ ch ? ch : "&nbsp;" }}
+                  </b-col>
+
+                </b-row>
+                <b-row class="lyric">
+                  <b-col
+                    class="lyric-sentence"
+                    :key="lindex"
+                    v-for="(ly, lindex) of bar.lyric"
+                  >
+                    {{ ly ? ly : "&nbsp;" }}
+                  </b-col>
+                </b-row>
+              </b-col>
+            </b-row>
+          </b-container>
         </b-col>
       </b-row>
     </b-container>
@@ -36,29 +55,57 @@
 </template>
 
 <script>
+import html2canvas from "html2canvas";
+
 export default {
   data() {
     return {
       editorText: "",
+      editorTitle: ""
     };
   },
-  computed:{
-    
-    formatted(){
-      let splitted = this.editorText.split("\n")
-      var arrayForReturn = []
+  computed: {
+    formattedObj() {
+      var splitted = this.editorText.split("\n\n");
+      let answerObj = [];
 
-      splitted.forEach((line,index,array) =>{
-        let text = line
-        let size = index != 0 ? 16 : 24
+      var splitted = splitted.length == 0 ? [this.editorText] : splitted;
 
+      splitted.forEach((elm, index, array) => {
+        console.log("elm", elm);
+        let lineObj = [];
 
-        arrayForReturn.push({
-          text: text,
-          size: size
-        })
-      })
-      return arrayForReturn
+        let chordsAndLyric = elm.split("\n");
+        let chords = chordsAndLyric[0];
+        let lyric = chordsAndLyric[1];
+
+        let splittedChords =
+          typeof chords != "undefined" ? chords.split("|") : [chords];
+        let splittedLyric =
+          typeof lyric != "undefined" ? lyric.split("|") : [lyric];
+
+        console.log("spltdchrd", splittedChords);
+        let bars = Math.max(splittedChords.length, splittedLyric.length);
+        console.log("bars", bars);
+
+        for (var i = 0; i < bars; i++) {
+          let nonnullChords =
+            typeof splittedChords[i] != "undefined" ? splittedChords[i] : "";
+          let splittedBarChords = nonnullChords.split(",");
+
+          let nonnullLyric =
+            typeof splittedLyric[i] != "undefined" ? splittedLyric[i] : "";
+          let splittedBarLyric = nonnullLyric.split(",");
+
+          lineObj.push({
+            chords: splittedBarChords,
+            lyric: splittedBarLyric
+          });
+        }
+        answerObj.push(lineObj);
+      });
+      console.log(answerObj);
+      return answerObj;
     }
   }
 };
@@ -67,7 +114,7 @@ export default {
 <style scoped>
 .header {
   background-color: var(--cyan);
-  height: 8vh;
+  height: 10vh;
 }
 .editor,
 .canvas,
@@ -75,5 +122,36 @@ export default {
   width: 100%;
   height: 88vh;
   border: 1px solid #ccc;
+}
+.bar{
+  border-right: #ccc 1px solid;
+}
+.bar:last-of-type{
+  border-right: none;
+}
+.chords {
+  color: var(--gray);
+  margin-bottom: 0.1rem;
+  line-height: 1;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+.chord {
+  flex-grow: 1;
+  text-align: left;
+}
+.lyric {
+  color: var(--dark);
+  margin-bottom: 0.5rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+.lyric-sentence {
+  flex-grow: 1;
+  text-align: left;
 }
 </style>
